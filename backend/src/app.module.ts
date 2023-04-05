b9a56434e686b87b021027157ebcb1bd0b6a096d
spark323 <spark323@gmail.com>
2023-04-05 16:38:26 +0900

    remove dist

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -16,7 +16,7 @@ import { UsersModule } from './users/users.module';
       database: process.env.DB_NAME,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      entities: ['dist/**/entities/**/*.entity{.ts,.js}'],
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
       logging: true,
     }),
