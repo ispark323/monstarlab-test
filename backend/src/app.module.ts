2ebff8d750cbc00b39fdb23f46c699cc40849032
spark323 <spark323@gmail.com>
2023-04-05 18:14:33 +0900

    add auth, users

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -19,6 +21,8 @@ import { AppService } from './app.service';
       synchronize: true,
       logging: true,
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
