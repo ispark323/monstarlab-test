8d478411b5b0521f291574c9eced406fe956bcfc
spark323 <spark323@gmail.com>
2023-04-05 23:07:48 +0900

    add TODO

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -9,6 +9,8 @@ export class AuthService {
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
     if (user && user.password === pass) {
+      // TODO: bcrypt
+      // https://zenn.dev/uttk/articles/9095a28be1bf5d
       const { password, ...result } = user;
       return result;
     }
